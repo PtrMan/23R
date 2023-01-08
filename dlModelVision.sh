74557dc14ac52700fa6c288f8ea51dac2802fbf8
PtrMan <1067920+PtrMan@users.noreply.github.com>
2023-01-08 03:39:43 +0100

    create: dlModelVision.sh

---
+++ dlModelVision.sh
@@ -0,0 +1,3 @@
+
+# script to download current model for vision
+wget https://gist.githubusercontent.com/PtrMan/a1be92243c8b2d228f167470190a29cf/raw/2371d53f700b5c20a0628596ab6eaa7e4477ae3b/snnweights_12u_12u_30122022.txt
