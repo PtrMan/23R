1bd4b6a73680cf405532bc9da1a0f900d45f8d37
PtrMan <1067920+PtrMan@users.noreply.github.com>
2023-02-12 11:15:47 +0100

    update: iterating over images

--- a.cpp
+++ a.cpp
@@ -1,6 +1,10 @@
 // compile with
 //    g++ a.cpp -o a `pkg-config --cflags --libs opencv`
 
+// for string conversation
+#include <sstream>
+#include <iomanip>
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -19,6 +23,14 @@ using namespace cv;
 //extern void visionSys0process0Cpp(tyObject_VisionSys0__69b9cVmnf9agBXMCdAtUelPgg*, NF*, NF*);
 
 
+// helper to convert int to string with leading zeros
+std::string convIntToStrLeadingZeros(int v, int width) {
+    // from https://stackoverflow.com/a/225435/388614
+    std::stringstream ss;
+    ss << std::setw(width) << std::setfill('0') << v;
+    return ss.str();
+}
+
 
 int main()
 {
@@ -27,36 +39,58 @@ int main()
 
     tyObject_VisionSys0__69b9cVmnf9agBXMCdAtUelPgg* visionSys = visionSys0Create();
 
-    std::string image_path = samples::findFile("0.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
+    Mat imgGrayLast;
 
-    
-    // convert to grayscale
-    Mat imgGray;
-    cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
 
-    // read pixels
-    {
+    for(int currentImageNr=0; currentImageNr<100; currentImageNr++) {
+        // read image
+        std::string image_path = samples::findFile(convIntToStrLeadingZeros(currentImageNr, 5)+".jpg");
+        Mat img = imread(image_path, IMREAD_COLOR);
+        if(img.empty())
+        {
+            std::cout << "Could not read the image: " << image_path << std::endl;
+            return 1;
+        }
+
+        
+        // convert to grayscale
+        Mat imgGray;
+        cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+        
+        if (currentImageNr==0) {
+            imgGrayLast = imgGray;
+        }
+
+        // read pixels
         // i is by column
         // j is by row
-        std::vector<double> arr;
+        std::vector<double> arrCurrent;
         for(int j=0;j<imgGray.rows;j++) {
             for (int i=0;i<imgGray.cols;i++) {
                 uchar val0 = imgGray.at<uchar>(j,i);
-                arr.push_back(val0/255.0);
+                arrCurrent.push_back(val0/255.0);
             }
         }
 
-        visionSys0process0Cpp(visionSys, &arr[0], &arr[0]);
+        std::vector<double> arrLast;
+        for(int j=0;j<imgGrayLast.rows;j++) {
+            for (int i=0;i<imgGrayLast.cols;i++) {
+                uchar val0 = imgGrayLast.at<uchar>(j,i);
+                arrLast.push_back(val0/255.0);
+            }
+        }
+
+
+        {
+            visionSys0process0Cpp(visionSys, &arrCurrent[0], &arrLast[0]);
+        }
+
+
+        imshow("Display window", img);
+        int k = waitKey(0); // Wait for a keystroke in the window
+
+        imgGrayLast = imgGray;
     }
 
-
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
