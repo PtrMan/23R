98050bab6100b547174b5c8f9a49c0c254bc19f9
PtrMan <1067920+PtrMan@users.noreply.github.com>
2023-02-15 10:39:16 +0100

    update: genImgs0: more complicated test, circular motion

--- genImg0/genImgs0.py
+++ genImg0/genImgs0.py
@@ -1,3 +1,5 @@
+import math
+
 from PIL import Image
 
 sprite0 = Image.open('./sprite0.png', 'r')
@@ -11,7 +13,13 @@ for itFrame in range(15):
     canvas = Image.new('RGBA', (128, 80), (0, 0, 0, 255))
     #bg_w, bg_h = canvas.size
     #offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-    offset = (10+int(itFrame*5), 10+int(itFrame*3))
+    
+    if False: # linear motion TEST
+        offset = (10+itFrame*5, 10+itFrame*3)
+    else:
+        offset = (50.0+math.cos(itFrame * 0.08)*20.0, 30.0+math.sin(itFrame * 0.08)*20.0)
+    
+    offset = (int(offset[0]), int(offset[1]))
 
     resizedSprite = sprite0.resize((20,20))
     canvas.paste(resizedSprite, offset)
