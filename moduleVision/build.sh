c39812c235dcd8d4af7eebbb6fe45eb11bfd536b
PtrMan <1067920+PtrMan@users.noreply.github.com>
2023-04-18 21:17:58 +0200

    update: vision: build in release mode for hopefully more speed

--- moduleVision/build.sh
+++ moduleVision/build.sh
@@ -1,5 +1,5 @@
 rm ./a
 
 # compile Nim to C++ target
-nim cpp --noMain --noLinking --header:visionSys0.h visionSys0.nim    && \
+nim cpp -d:release --noMain --noLinking --header:visionSys0.h visionSys0.nim    && \
 g++  -o a `pkg-config --cflags --libs opencv`      -I/home/r0b3/root/installedPrograms/nim-1.6.10/lib  -I$HOME/.cache/nim/visionSys0_d $HOME/.cache/nim/visionSys0_d/*.cpp a.cpp
