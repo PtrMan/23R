dfc23240753116c92d8b661942b94d5561f6ba87
PtrMan <1067920+PtrMan@users.noreply.github.com>
2023-04-26 12:12:56 +0200

    add: NLP: script to install requirements

---
+++ moduleNlp4/installRequirements.sh
@@ -0,0 +1,8 @@
+# needs model from https://github.com/lm-sys/FastChat  
+
+# TODO
+#   (follow instructions to get the model TODO)
+
+
+pip install accelerate
+pip3 install git+https://github.com/huggingface/transformers
